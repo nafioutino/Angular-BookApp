5718458857d9381a0ab48a6f0e2fd1b25e8a6c8d
Abdoul-Nafiou <nafiou531@gmail.com>
2024-08-06 14:30:21 +0100

    book app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { BookHeaderComponent } from './book-header/book-header.component';
+import { BookFooterComponent } from './book-footer/book-footer.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookHeaderComponent,
+    BookFooterComponent,
+    BookListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/book-header/book-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-header',
+  templateUrl: './book-header.component.html',
+  styleUrl: './book-header.component.css'
+})
+export class BookHeaderComponent {
+
+}
